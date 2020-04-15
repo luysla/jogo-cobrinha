3938b28916ae7c4ca9b03a25aaf0882d3bce1b1a
luysla <dyana.tavares1@gmail.com>
2020-04-15 13:57:15 -0400

    Adicionei função para criar a cobrinha

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var box = 32;
+var cobrinha = [];
+cobrinha[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
 
-function criaBackground() {
+function criarBackground() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criaBackground();
+function criarCobrinha() {
+  for (i = 0; i < cobrinha.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
+  }
+}
+
+criarBackground();
+criarCobrinha();
