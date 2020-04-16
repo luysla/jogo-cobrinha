f5d1cc8f7d309cefd01ae9f6bb6cfbe4c4545559
luysla <dyana.tavares1@gmail.com>
2020-04-15 23:36:36 -0400

    Adicionei direções e condições para que a cobrinha não saia do canvas

--- main.js
+++ main.js
@@ -20,24 +20,40 @@ function criarCobrinha() {
   }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.keyCode == 37 && direcao != "right") direcao = "left";
+  if (event.keyCode == 38 && direcao != "down") direcao = "up";
+  if (event.keyCode == 39 && direcao != "left") direcao = "right";
+  if (event.keyCode == 40 && direcao != "up") direcao = "down";
+}
+
 function iniciarJogo() {
+  if (cobrinha[0].x > 15 * box && direcao == "right") cobrinha[0].x = 0;
+  if (cobrinha[0].x < 0 && direcao == "left") cobrinha[0].x = 16 * box;
+  if (cobrinha[0].y > 15 * box && direcao == "down") cobrinha[0].y = 0;
+  if (cobrinha[0].y < 0 && direcao == "up") cobrinha[0].y = 16 * box;
+
   criarBackground();
   criarCobrinha();
 
   let cobrinhaPosX = cobrinha[0].x;
   let cobrinhaPosY = cobrinha[0].y;
+
+  if (direcao == "right") cobrinhaPosX += box;
+  if (direcao == "left") cobrinhaPosX -= box;
+  if (direcao == "up") cobrinhaPosY -= box;
+  if (direcao == "down") cobrinhaPosY += box;
+
+  cobrinha.pop();
+
   let cabecaCobrinha = {
     x: cobrinhaPosX,
     y: cobrinhaPosY,
   };
 
-  if (direcao == "right") cobrinhaPosX += box;
-  if (direcao == "left") cobrinhaPosY -= box;
-  if (direcao == "up") cobrinhaPosY -= box;
-  if (direcao == "down") cobrinhaPosY += box;
-
-  cobrinha.pop();
-  snake.unshift(cabecaCobrinha);
+  cobrinha.unshift(cabecaCobrinha);
 }
 
 var jogo = setInterval(iniciarJogo, 100);
